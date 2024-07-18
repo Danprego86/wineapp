d755a23d99297f38140ef2f33fb062f57fa4d462
Daniel Pre <danpreacu@gmail.com>
2024-07-18 16:09:14 -0500

    Migracion de base de datos

--- app/src/main/java/com/example/wineapp/WineDatabase.kt
+++ app/src/main/java/com/example/wineapp/WineDatabase.kt
@@ -4,7 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [Wine::class], version = 1)
+@Database(entities = [Wine::class], version = 2
+)
 @TypeConverters(WineConverter::class)
 abstract class WineDatabase: RoomDatabase() {
     abstract fun wineDao(): WineDao
